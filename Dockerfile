7ee5812386db055d9123780bcb5716c81bd7237e
Oleksii Trekhleb <trehleb@gmail.com>
2017-08-30 06:33:01 +0300

    Do smth.

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,17 @@
-# Use an official Python runtime as a parent image
+# Use an official Python runtime as a parent image.
 FROM python:2.7-slim
 
-# Set the working directory to /app
+# Set the working directory to /app.
 WORKDIR /app
 
-# Copy the current directory contents into the container at /app
+# Copy the current directory contents into the container at /app.
 ADD . /app
 
-# Install any needed packages specified in requirements.txt
+# Install any needed packages specified in requirements.txt.
 RUN pip install -r requirements.txt
 
-# Make port 80 available to the world outside this container
+# Make port 80 available to the world outside this container.
 EXPOSE 80
 
-# Define environment variable
-ENV NAME World
-
-# Run app.py when the container launches
+# Run app.py when the container launches.
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,24 +1,21 @@
 from flask import Flask
 from redis import Redis, RedisError
-import os
-import socket
 
-# Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
-
+redis = Redis(host="redis")
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     try:
         visits = redis.incr("counter")
     except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
+        visits = "<i>Can't connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
+    html = "<h3>Hello Docker!</h3>" \
            "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+    return html.format(visits=visits)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
